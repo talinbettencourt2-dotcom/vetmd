cbbfff64763be67327cedc68b5f9b79e2e952162
talinbettencourt2-dotcom <talinbettencourt2@gmail.com>
2026-03-24 18:30:04 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,13 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/sign-in", "/privacy", "/terms"],
+  publicRoutes: ["/", "/sign-in(.*)", "/privacy", "/terms", "/api/(.*)"],
+  afterAuth(auth, req) {
+    // If signed in and on sign-in page, redirect to /app
+    if (auth.userId && req.nextUrl.pathname === "/sign-in") {
+      return Response.redirect(new URL("/app", req.url));
+    }
+  }
 });
 
 export const config = {
