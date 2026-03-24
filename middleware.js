809bd71496182c9efad3037f5b58ae84cddd0fd2
talinbettencourt2-dotcom <talinbettencourt2@gmail.com>
2026-03-24 18:05:11 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,10 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware()
+export default authMiddleware({
+  publicRoutes: ["/", "/sign-in", "/privacy", "/terms"],
+});
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
   ],
-}
+};
